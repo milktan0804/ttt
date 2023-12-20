6af7a7a4c11ec9de1987b41150fbadf7f50e6a8e
milktan0804 <106955158+milktan0804@users.noreply.github.com>
2023-12-20 21:19:50 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './src/config/firebase';
+import RootNavigation from './src/navigations/';
+
+export default function App() {
+  return (
+    <RootNavigation />
+  );
+}
